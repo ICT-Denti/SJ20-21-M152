4c84816a99df89c938133a8bf8deb663eb3f0733
Alexander Denti <alexander_denti@sluz.ch>
2022-06-21 02:04:14 +0200

    Noten System hinzugefügt.

--- m152/VueJS_Project/notenverwaltungstool/src/components/main.vue
+++ m152/VueJS_Project/notenverwaltungstool/src/components/main.vue
@@ -1,23 +1,137 @@
 <template>
 <main class="grid-container">
-  <div class="fach1"></div>
-  <div class="fach2"></div>
-  <div class="fach3"></div>
-  <div class="fach4"></div>
-  <div class="fach5"></div>
-  <div class="fach6"></div>
-  <div class="fach7"></div>
-  <div class="fach8"></div>
+  <div class="fach1">
+    <h2>ABU</h2>
+      <form @submit.prevent="addNote">
+    <input v-model="newNote">
+    <button>Add Note</button>    
+  </form>
+  <ul>
+    <li v-for="note in notes" :key="note.id">
+      {{ note.text }}
+      <button @click="removeNote(note)">X</button>
+    </li>
+  </ul>
+  </div>
+  <div class="fach2">
+    <h2>M306</h2>
+      <form @submit.prevent="addNote">
+    <input v-model="newNote">
+    <button>Add Note</button>    
+  </form>
+  <ul>
+    <li v-for="note in notes" :key="note.id">
+      {{ note.text }}
+      <button @click="removeNote(note)">X</button>
+    </li>
+  </ul>
+  </div>
+  <div class="fach3">
+    <h2>NW</h2>
+      <form @submit.prevent="addNote">
+    <input v-model="newNote">
+    <button>Add Note</button>    
+  </form>
+  <ul>
+    <li v-for="note in notes" :key="note.id">
+      {{ note.text }}
+      <button @click="removeNote(note)">X</button>
+    </li>
+  </ul>
+  </div>
+  <div class="fach4">
+    <h2>M151</h2>
+      <form @submit.prevent="addNote">
+    <input v-model="newNote">
+    <button>Add Note</button>    
+  </form>
+  <ul>
+    <li v-for="note in notes" :key="note.id">
+      {{ note.text }}
+      <button @click="removeNote(note)">X</button>
+    </li>
+  </ul>
+  </div>
+  <div class="fach5">
+    <h2>M152</h2>
+      <form @submit.prevent="addNote">
+    <input v-model="newNote">
+    <button>Add Note</button>    
+  </form>
+  <ul>
+    <li v-for="note in notes" :key="note.id">
+      {{ note.text }}
+      <button @click="removeNote(note)">X</button>
+    </li>
+  </ul>
+  </div>
+  <div class="fach6">
+    <h2>M153</h2>
+      <form @submit.prevent="addNote">
+    <input v-model="newNote">
+    <button>Add Note</button>    
+  </form>
+  <ul>
+    <li v-for="note in notes" :key="note.id">
+      {{ note.text }}
+      <button @click="removeNote(note)">X</button>
+    </li>
+  </ul>
+  </div>
+  <div class="fach7">
+    <h2>Sport</h2>
+      <form @submit.prevent="addNote">
+    <input v-model="newNote">
+    <button>Add Note</button>    
+  </form>
+  <ul>
+    <li v-for="note in notes" :key="note.id">
+      {{ note.text }}
+      <button @click="removeNote(note)">X</button>
+    </li>
+  </ul>
+  </div>
+  <div class="fach8">
+    <h2>W&R</h2>
+      <form @submit.prevent="addNote">
+    <input v-model="newNote">
+    <button>Add Note</button>    
+  </form>
+  <ul>
+    <li v-for="note in notes" :key="note.id">
+      {{ note.text }}
+      <button @click="removeNote(note)">X</button>
+    </li>
+  </ul>
+  </div>
 </main>
 </template>
 
 
-<script>
-export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
-  }
+<script setup>
+import { ref } from 'vue';
+
+
+// give each note a unique id
+let id = 0
+
+const newNote = ref('')
+const notes = ref([
+  { id: id++, text: '4.0' },
+  { id: id++, text: '6.0' },
+  { id: id++, text: '5.5' }
+])
+
+function addNote() {
+  notes.value.push({ id: id++, text: newNote.value })
+  newNote.value = ''
+}
+
+function removeNote(note) {
+  notes.value = notes.value.filter((t) => t !== note)
 }
 </script>
 
+<style>
+  @import '../assets/style.css';
+</style>
