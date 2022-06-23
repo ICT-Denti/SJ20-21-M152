996fa587c1db3459da5ebe8f766352ee45c8a473
Alexander Denti <alexander_denti@sluz.ch>
2022-06-23 03:53:22 +0200

    Projekt soweit fertig.

--- m152/VueJS_Project/notenverwaltungstool/src/components/main.vue
+++ m152/VueJS_Project/notenverwaltungstool/src/components/main.vue
@@ -1,134 +1,85 @@
 <template>
-<main class="grid-container">
-  <div class="fach">
-    <h2>ABU</h2>
-  <form @submit.prevent="addNote">
-    <input v-model="newNote">
-    <button>Add Note</button>    
-  </form>
-  <ul>
-    <li v-for="note in notes" :key="note.id">
-      {{ note.text }}
-      <button @click="removeNote(note)">X</button>
-    </li>
-  </ul>
-  </div>
-  <div class="fach">
-    <h2>M306</h2>
-  <form @submit.prevent="addNote">
-    <input v-model="newNote">
-    <button>Add Note</button>    
-  </form>
-  <ul>
-    <li v-for="note in notes" :key="note.id">
-      {{ note.text }}
-      <button @click="removeNote(note)">X</button>
-    </li>
-  </ul>
-  </div>
-  <div class="fach">
-    <h2>NW</h2>
-  <form @submit.prevent="addNote">
-    <input v-model="newNote">
-    <button>Add Note</button>    
-  </form>
-  <ul>
-    <li v-for="note in notes" :key="note.id">
-      {{ note.text }}
-      <button @click="removeNote(note)">X</button>
-    </li>
-  </ul>
-  </div>
-  <div class="fach">
-    <h2>M151</h2>
-  <form @submit.prevent="addNote">
-    <input v-model="newNote">
-    <button>Add Note</button>    
-  </form>
-  <ul>
-    <li v-for="note in notes" :key="note.id">
-      {{ note.text }}
-      <button @click="removeNote(note)">X</button>
-    </li>
-  </ul>
-  </div>
-  <div class="fach">
-    <h2>M152</h2>
-  <form @submit.prevent="addNote">
-    <input v-model="newNote">
-    <button>Add Note</button>    
-  </form>
-  <ul>
-    <li v-for="note in notes" :key="note.id">
-      {{ note.text }}
-      <button @click="removeNote(note)">X</button>
-    </li>
-  </ul>
-  </div>
-  <div class="fach">
-    <h2>M153</h2>
-  <form @submit.prevent="addNote">
-    <input v-model="newNote">
-    <button>Add Note</button>    
-  </form>
-  <ul>
-    <li v-for="note in notes" :key="note.id">
-      {{ note.text }}
-      <button @click="removeNote(note)">X</button>
-    </li>
-  </ul>
-  </div>
-  <div class="fach">
-    <h2>Sport</h2>
-  <form @submit.prevent="addNote">
-    <input v-model="newNote">
-    <button>Add Note</button>    
-  </form>
-  <ul>
-    <li v-for="note in notes" :key="note.id">
-      {{ note.text }}
-      <button @click="removeNote(note)">X</button>
-    </li>
-  </ul>
-  </div>
-  <div class="fach">
-    <h2>W&R</h2>
-  <form @submit.prevent="addNote">
-    <input v-model="newNote">    
-    <button>Add Note</button> 
-  </form>
-  <ul>
-    <li v-for="note in notes" :key="note.id">
-      {{ note.text }}
-      <button @click="removeNote(note)">X</button>
-    </li>
-  </ul>
-  </div>
-</main>
+  <main class="grid-container">
+    <div class="fach" v-for="(noten, fach) in Faecher" :key="fach">
+      <h2>
+        {{ fach }}
+      </h2>
+      <form @keyup.enter="addNote($event, fach)">
+        <input type="number" min="0" max="6"/>
+         <button>Note hinzufügen</button> 
+      </form>
+      <ul>
+        <li v-for="(note, index) in noten.noten" :key="index">
+          {{ parseFloat(note).toFixed(2) }}
+          <button @click="removeNote(fach, index)">X</button>
+        </li>
+        <li>
+          <p class="schnitt"> Schnitt: {{ parseFloat(calculateAverageGrade(fach)).toFixed(2) }}</p>
+        </li>
+      </ul>
+    </div>
+      <form @keyup.enter="addSubject($event)">
+        <label type="text">Name für Neues Fach: </label>
+        <input type="text"/>
+        <button>hinzufügen</button> 
+      </form>
+    <div>
+      <p>Gesamter Notendurchschnitt:</p>
+      <p class="schnitt">{{ parseFloat(roundHalf(calculateAverageGradeFromAllSubjects())).toFixed(2) }}</p>
+    </div>
+  </main>
 </template>
 
-
 <script setup>
+
 import { ref } from 'vue';
 
+const Faecher = ref({
+  ABU: { noten: [3, 4.5, 5.3] },
+  NW: { noten: [6, 2.7, 3.5] },
+  Sport: { noten: [1.1, 3.6] },
+  WuR: { noten: [5.3, 4.4] },
+  M151: { noten: [2, 5, 4] },
+  M152: { noten: [4, 3.8] },
+  M153: { noten: [5, 5.8] },
+  M306: { noten: [6, 5.9] },
+});
 
-// give each note a unique id
-let id = 0
-
-const newNote = ref('')
-const notes = ref([
-  { id: id++, text: '3.0' },
-  { id: id++, text: '6.0' },
-  { id: id++, text: '5.5' }
-])
-
-function addNote() {
-  notes.value.push({ id: id++, text: newNote.value })
-  newNote.value = ''
+function removeNote(fach, note) {
+  Faecher.value[fach].noten.splice(note, 1);
 }
 
-function removeNote(note) {
-  notes.value = notes.value.filter((t) => t !== note)
+function addNote(x, fach) {
+  let newNote = Math.min(6, Math.max(1, parseInt(x.target.value)));
+  Faecher.value[fach].noten.push(newNote);
+}
+
+function calculateAverageGrade(fach) {
+  let sum = 0;
+  for (let note of Faecher.value[fach].noten) {
+    sum += note;
+  }
+  return sum / Faecher.value[fach].noten.length;
+}
+
+function calculateAverageGradeFromAllSubjects() {
+  let sum = 0;
+  let count = 0;
+  for (let fach in Faecher.value) {
+    for (let note of Faecher.value[fach].noten) {
+      sum += note;
+      count++;
+    }
+  }
+  return sum / count;
+}
+
+function addSubject(x) {
+  Faecher.value[x.target.value] = { noten: [] };
+}
+
+function roundHalf(num) {
+    return Math.round(num*2)/2;
 }
 </script>
 
